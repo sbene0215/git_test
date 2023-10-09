210e9231473eb551d4d2a54c106406003d46c6db
user <sk87732925@gmail.com>
2023-10-09 22:01:08 +0900

    modify new file

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Wolrd!");
+        System.out.println("Hello, World!");
+        System.out.println(5 * 8);
     }
 }
